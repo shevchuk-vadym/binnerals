424234d15fa52090cb5ad13e9c6688f47ed61c55
Shevchuk-Vadym <lorelliand@gmail.com>
2022-02-04 20:28:12 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import Login from './Components/Login/Login';
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
